4637839fceae02cf6b183904b2733b8bfa1c96d3
Rene van Osnabrugge <rvanosnabrugge@live.com>
2018-03-21 00:27:00 +0100

    Added download and update of Security Groups + Modified Readme

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # azure-security-as-code
 Scripts to define your azure security governance as code and avoid manual settings of permissions and avoiding configuration drift
 
-## Usage ##
+## Usage in general ##
+
 * Login with az login
 * import powershell script
 
 ```
 . .\resourcegroups-module.ps1
 ```
+
+## Usage Resource Group Download ##
 
 * Run 1 resource group or all
 
@@ -17,4 +20,23 @@ Download-AllResourceGroups
 #or
 
 Download-ResourceGroupYaml -resourcegroup "resourcegroupName"
+```
+
+## Usage Security Group Download ##
+
+* Run 1 security group or all
+* creates and looks for files in directory ad-group
+
+```powershell
+Download-AllSecurityGroups
+
+#or
+
+Download-SecurityGroupYaml -securitygroup "securitygroupName"
+```
+
+* Update yaml file with new users
+
+```powershell
+Update-SecurityGroup -securitygroup "securitygroupName"
 ```
---
+++ SecurityGroups-Module.ps1
@@ -0,0 +1,89 @@
+function Download-SecurityGroupYaml
+{
+    param
+    (
+        [string] $securityGroup
+    )
+
+    $users = "$(az ad group member list --group "$($securityGroup)")"
+    $users = ConvertFrom-Json $users
+
+    
+    $userArray = @()
+
+    foreach($u in $users)
+    {
+        $userDict = [ordered]@{userPrincipalName = $u.userPrincipalName
+                                objectId = $u.objectId
+                                displayName = $u.displayName}
+        $userArray += $userDict
+    }
+    
+    $rgDict = [ordered]@{}
+    $rgDict.Add('SecurityGroup',$securityGroup)
+    if($userArray -ne $null)
+    {
+        $rgDict.Add('members',$userArray)
+    }
+
+
+    $path = Join-Path $PSScriptRoot -ChildPath "ad-groups"
+    New-Item -ItemType Directory -Path $path -Force
+    $file = Join-Path $path -ChildPath "$($securityGroup).yml"
+    ConvertTo-YAML $rgDict -OutFile $file -Force
+}
+
+
+function Update-SecurityGroup
+{
+    param
+    (
+        [string] $securityGroup
+    )
+
+    
+    $path = Join-Path $PSScriptRoot -ChildPath "ad-groups"
+    $file = Join-Path $path -ChildPath "$($securityGroup).yml"
+    $yamlContent = Get-Content -Path $file -Raw
+    $secgroupmembers = ConvertFrom-Yaml -Yaml $yamlContent
+    $secgroupmembers = ConvertFrom-Json $secgroupmembers
+    
+
+    $secGroup = "$(az ad group show --group "$($securityGroup)")"
+    $secGroup = ConvertFrom-Json $secGroup
+
+    foreach($m in $secgroupmembers["members"])
+    {
+        if($m.objectId -ne $null) {
+            $user = "$(az ad user show --upn-or-object-id $m.objectId)"
+
+        }
+        else {
+            $user = "$(az ad user show --upn-or-object-id $m.userPrincipalName)"
+
+        }
+        $user = ConvertFrom-Json $user
+        $result = "$(az ad group member check --group "$($secGroup.ObjectID)" --member-id "$($user.ObjectID)")"
+        $result = ConvertFrom-Json $result
+        
+        if ($($result.value) -eq $false) {
+            Write-Host "Adding $($user.displayName) ($($m.userPrincipalName)) to Security Group $($securityGroup)"
+            $added = "$(az ad group member add --group "$($secGroup.ObjectID)" --member-id "$($user.ObjectID)")"
+        }
+        else {
+            Write-Host "User $($user.displayName) ($($m.userPrincipalName)) already exists in Security Group $($securityGroup)"
+            
+        }
+    }
+}
+
+function Download-AllSecurityGroups
+{
+    $secGroups = "$(az ad group list --output json)"
+    $secGroups = ConvertFrom-Json $secGroups
+
+
+    foreach ($sg in $secGroups) {
+        Download-SecurityGroupYaml -securityGroup $sg.displayName
+    }
+}
