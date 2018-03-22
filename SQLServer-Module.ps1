e215772b3a5466a09f69dc0f968b9df6b38eca9f
Rene van Osnabrugge <rvanosnabrugge@live.com>
2018-03-22 15:12:13 +0100

    Refactored for Reuse

--- SQLServer-Module.ps1
+++ SQLServer-Module.ps1
@@ -1,8 +1,68 @@
 #Import Helpers
 . .\SecurityAsCode-Helpers.ps1
 
-function New-Asac-SQLServer
-{
+function _GetSQLServerDictionary {
+    param
+    (
+        [string] $sqlservername,
+        [string] $sqladminlogin
+    )
+
+    $sqlDict = [ordered]@{  sqlservername = $sqlservername
+        sqladminlogin = $sqladminlogin
+    }
+
+    return $sqlDict
+}
+
+
+function _GetSQLServerPasswordDictionary {
+    param
+    (
+        [string] $secretkey,
+        [string] $keyvaultname,
+        [string] $keyvaultresourcegroup
+    )
+
+    $sqlpasswordDict = [ordered]@{ secretkey = $secretkey
+        keyvaultname = $keyvaultname
+        keyvaultresourcegroup = $keyvaultresourcegroup
+    }
+
+
+    return $sqlpasswordDict
+}
+
+
+function _GetSQLServerFirewallDict {
+    param
+    (
+        [string] $rulename,
+        [string] $startip,
+        [string] $endip
+    )
+    $firewallDict = [ordered]@{rulename = $rulename
+        startip = $startip
+        endip = $endip
+    }
+
+    return $firewallDict
+}
+
+function _GetSQLServerADAdminDict {
+    param
+    (
+        [string] $userPrincipal,
+        [string] $principalId
+    )
+
+    $adadminDict = [ordered]@{userPrincipal = $userPrincipal
+        principalId = $principalId
+    }
+
+    return $adadminDict
+}
+function New-Asac-SQLServer {
     param
     (
         [string] $sqlservername,
@@ -11,45 +71,44 @@ function New-Asac-SQLServer
 
     $outputPath = _Get-Asac-OutputPath -outputPath $outputPath
 
-    $sqlDict = [ordered]@{sqlservername = "sqlserv123 #SQL Server Name"
-                          sqladminlogin = "[generated] #username of SA when creating SQL Server"}
+    $sqlDict = _GetSQLServerDictionary -sqlservername  "sqlserv123 #SQL Server Name" -sqladminlogin "[generated] #username of SA when creating SQL Server"
 
-    $sqlpasswordDict = [ordered]@{ secretkey = "sql-bigboss #Keyname in keyvault"
-                                 keyvaultname = "kvPlatform123 #Name of the keyvault where password resides"
-                                 keyvaultresourcegroup = "rgTest123 #Name of the resource group where the keyvault resides"}
+    $sqlpasswordDict = _GetSQLServerPasswordDictionary -secretkey "sql-bigboss #Keyname in keyvault" `
+        -keyvaultname"kvPlatform123 #Name of the keyvault where password resides" `
+        -keyvaultresourcegroup "rgTest123 #Name of the resource group where the keyvault resides"
 
-    $adadminArray = @()
-    for ($i = 0; $i -lt 2;$i++) {
 
-    $adadminDict = [ordered]@{userPrincipal = "John@domain.com #User or GroupName"
-                              principalId = "7a2eddfe-341e-4c3b-8917-58a678249a81 #Object ID of User or GroupName. If empty name will be used"}
-    $adadminArray +=$adadminDict
-    }
+$adadminArray = @()
+for ($i = 0; $i -lt 2; $i++) {
 
-    $firewallArray = @()
-    for ($i = 0; $i -lt 2;$i++) {
+    $adadminDict = _GetSQLServerADAdminDict -userPrincipal "John@domain.com #User or GroupName" -principalId "7a2eddfe-341e-4c3b-8917-58a678249a81 #Object ID of User or GroupName. If empty name will be used"
+    $adadminArray += $adadminDict
+}
 
-        $firewallDict = [ordered]@{rulename = "Name of Rule"
-                                  startip = "10.1.1.1"
-                                  endip = "10.1.1.2"}
-        $firewallArray += $firewallDict
-        }
+$firewallArray = @()
+for ($i = 0; $i -lt 2; $i++) {
 
-    $sqlDict.Add('sqladminpassword',$sqlpasswordDict)
-    $sqlDict.Add('ad-admins',$adadminArray)
-    $sqlDict.Add('firewallports',$firewallArray)
+    $firewallDict = _GetSQLServerFirewallDict -rulename "Name of Rule" `
+        -startip "10.1.1.1" `
+        -endip "10.1.1.2"
     
-    $path = Join-Path $outputPath -ChildPath "sql"
-    New-Item $path -Force -ItemType Directory
-    $filePath = Join-Path $path -ChildPath "sql.$($sqlservername).yml"
-    Write-Host $filePath
-    ConvertTo-YAML $sqlDict > $filePath                               
+    $firewallArray += $firewallDict
+}
+
+$sqlDict.Add('sqladminpassword', $sqlpasswordDict)
+$sqlDict.Add('ad-admins', $adadminArray)
+$sqlDict.Add('firewallports', $firewallArray)
+    
+$path = Join-Path $outputPath -ChildPath "sql"
+New-Item $path -Force -ItemType Directory
+$filePath = Join-Path $path -ChildPath "sql.$($sqlservername).yml"
+Write-Host $filePath
+ConvertTo-YAML $sqlDict > $filePath                               
 
 
 }
 
-function Get-Asac-SQLServer
-{
+function Get-Asac-SQLServer {
     param
     (
         [string] $sqlservername,
@@ -61,13 +120,13 @@ function Get-Asac-SQLServer
     $sql = Invoke-AzCommandLine -azCommandLine "az sql server show --name $($sqlservername) --output json"
     
     $sqlDict = [ordered]@{     sqlservername = $sql.name
-                               sqladminlogin = $sql.administratorLogin
-                               sqladminpassword = $role.properties.roleDefinitionName}
+        sqladminlogin = $sql.administratorLogin
+        sqladminpassword = $role.properties.roleDefinitionName}
     
     
     $rgDict = [ordered]@{}
-    $rgDict.Add('resourcegroup',$rg.name)
-    $rgDict.Add('rbac',$rbacArray)
+    $rgDict.Add('resourcegroup', $rg.name)
+    $rgDict.Add('rbac', $rbacArray)
 
     $path = Join-Path $outputPath -ChildPath "rg"
     New-Item $path -Force -ItemType Directory
@@ -77,15 +136,13 @@ function Get-Asac-SQLServer
 }
 
 
-function Get-Asac-AllSQLServers
-{ 
+function Get-Asac-AllSQLServers { 
     param
     (
         [string] $outputPath
     )
 
-    if ($outputPath -eq "" -or $outputPath -eq $null)  
-    {
+    if ($outputPath -eq "" -or $outputPath -eq $null) {
         $outputPath = $PSScriptRoot
     }
 
@@ -97,8 +154,7 @@ function Get-Asac-AllSQLServers
     }
 }
 
-function Process-Asac-SQLServer
-{
+function Process-Asac-SQLServer {
     param
     (
         [string] $sqlservername,
@@ -106,8 +162,7 @@ function Process-Asac-SQLServer
         
     )
 
-    if ($basePath -eq "" -or $basePath -eq $null)  
-    {
+    if ($basePath -eq "" -or $basePath -eq $null) {
         $basePath = $PSScriptRoot
     }
 
