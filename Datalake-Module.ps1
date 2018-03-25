5803bd0c99f3660dc7e038437465885caae64de4
Rene van Osnabrugge <rvanosnabrugge@live.com>
2018-03-25 10:25:05 +0200

    Sample commands for DataLake

---
+++ Datalake-Module.ps1
@@ -0,0 +1,5 @@
+az dls fs access set-entry --account accountdls1 --path /dir/subdir --acl-spec 'user:username@domain.com:rwx'
+
+az dls fs access set-entry --acl-spec 'other:ecf90874-b567-4c39-9f1a-8e57ee3a8b8a:rwx' --account accountdls1 --path /aap/aapsub1
+
+az dls fs list --account accountdls1 --path \aap
